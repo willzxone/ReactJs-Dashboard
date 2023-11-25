473bc24a8861978ef3cc9a24221a04c3650d5041
willzxone <willzxone0987@gmail.com>
2023-11-26 02:28:01 +0500

    Product Info - Added

--- src/features/Dashboard/businessOverview/index.jsx
+++ src/features/Dashboard/businessOverview/index.jsx
@@ -1,13 +1,14 @@
 import BusinessDesign from "./businessDesign";
 import TeamMembers from "./teamMembers";
 import ControlCard from "./controlCard";
+import ProductInfo from "./productInfo";
 const BusinessOverview = () => {
   return (
     <div className="mt-10 mb-10 flex gap-10">
       <BusinessDesign />
       <TeamMembers />
       <ControlCard />
-      <BusinessDesign />
+      <ProductInfo />
     </div>
   );
 };
---
+++ src/features/Dashboard/businessOverview/productInfo/index.jsx
@@ -0,0 +1,47 @@
+import { Card } from "../../../../components/card";
+import StarbucksIcon from "../../../../assets/Icons/BusinessOverview/starbucks.svg";
+import WatchIcon from "../../../../assets/Icons/BusinessOverview/watch_later.svg";
+import FoodIcom from "../../../../assets/Icons/BusinessOverview/fastfood.svg";
+import StarbuckImage from "../../../../assets/Images/image.png";
+
+const ProductInfo = () => {
+  return (
+    <div className="w-1/4 relative">
+      <div className="relative w-full h-[185px] overflow-hidden">
+        <div className="relative rounded-2xl overflow-hidden h-[185px]">
+          {/* Image */}
+          <img
+            src={StarbuckImage}
+            className="rounded-2xl object-cover w-full h-full z-0"
+            alt="Description"
+          />
+
+          {/* Gradient Overlay */}
+          <div className="absolute inset-0 rounded-2xl backdrop-filter bg-gradient-to-b to-[#111c44cc] from-[#111c4400] z-10"></div>
+        </div>
+        <div className="absolute top-2 right-2 p-3 rounded-2xl w-fit backdrop-filter backdrop-blur-50 backdrop-brightness-100 bg-gradient-to-b from-[rgb(88,85,85)] to-[rgb(119,150,168)] z-10 ">
+          <img src={WatchIcon} className="z-10" />
+        </div>
+      </div>
+      <Card
+        className="flex-col absolute bottom-[0%] left-3 z-20"
+        leftIcon={<img src={StarbucksIcon} />}
+        heading={
+          <p className="[font-family:'DM_Sans-Bold',Helvetica] font-bold text-[#2b3674] text-[24px] tracking-[-0.48px] leading-[32px] mt-6">
+            Starbucks
+          </p>
+        }
+        value={
+          <>
+            <img src={FoodIcom} className="inline mr-2" />
+            <p className="[font-family:'DM_Sans-Medium',Helvetica] font-medium text-[#a3aed0] text-[14px] tracking-[-0.28px] leading-[24px] whitespace-nowrap inline">
+              10% cashback & off
+            </p>
+          </>
+        }
+      />
+    </div>
+  );
+};
+
+export default ProductInfo;
